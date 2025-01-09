1d1445477515cea82bf0b4497cb1fcb20c8ee17d
gabrilpedroti <gabrielpedroti@icloud.com>
2025-01-08 23:21:38 -0300

    Inicio do projeto

--- classificador_de_nivel_de_heroi/index.js
+++ classificador_de_nivel_de_heroi/index.js
@@ -1,2 +1,8 @@
-let mensagem = "Ola mundo!"
-console.log(mensagem)
+let heroName
+let heroXP
+let heroRank
+
+console.log("Bem-vindo ao classifacor de Rank!\nVou precisar de alguma informações..."
+)
+heroName = prompt("Qual o nome do seu herói?")
+heroXP = prompt("Qual o XP dele(a)?")
