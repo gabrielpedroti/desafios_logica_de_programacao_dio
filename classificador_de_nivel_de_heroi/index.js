d31364d6ccc657ba7c503597e359409e0127b4bd
gabrielpedroti <gabrielpedroti@icloud.com>
2025-01-10 16:57:47 -0300

    Estrutura de decisão OK

--- classificador_de_nivel_de_heroi/index.js
+++ classificador_de_nivel_de_heroi/index.js
@@ -3,8 +3,7 @@ let heroXP
 let heroRank
 
 heroName = "Hulk"
-heroXP = 1000
-heroRank = "Ferro"
+heroXP = 10651
 
 /*
 console.log("Bem-vindo ao classifacor de Rank!\nVou precisar de alguma informações...")
@@ -12,5 +11,23 @@ heroName = prompt("Qual o nome do seu herói?")
 heroXP = prompt("Qual o XP dele(a)?")
 */
 
-console.log("Calculando.../\n")
+if (heroXP <= 1000){
+    heroRank = "Ferro"
+}else if (heroXP > 1000 && heroXP <= 2000){
+    heroRank = "Bronze"
+}else if (heroXP > 2000 && heroXP <= 5000){
+    heroRank = "Prata"
+}else if (heroXP >5000 && heroXP <= 7000){
+    heroRank = "Ouro"
+}else if (heroXP > 7000 && heroXP <= 8000){
+    heroRank = "Platina"
+}else if (heroXP > 8000 && heroXP <= 9000){
+    heroRank = "Ascendente"
+}else if (heroXP > 9000 && heroXP <= 10000){
+    heroRank = "Imortal"
+}else if (heroXP > 10000){
+    heroRank = "Radiante"
+}
+
+console.log("Calculando...\n")
 console.log("O rank do(a) " + heroName + " é: " + heroRank)
