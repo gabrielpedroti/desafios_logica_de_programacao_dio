9578e91fcf4423bd7a1c1cb52b04661125877551
gabrilpedroti <gabrielpedroti@icloud.com>
2025-01-11 22:17:19 -0300

    Projeto finalizado

--- classificador_de_nivel_de_heroi/index.js
+++ classificador_de_nivel_de_heroi/index.js
@@ -2,14 +2,10 @@ let heroName
 let heroXP
 let heroRank
 
-heroName = "Hulk"
-heroXP = 10651
+heroName = "Reyna"
+heroXP = 6742
 
-/*
-console.log("Bem-vindo ao classifacor de Rank!\nVou precisar de alguma informações...")
-heroName = prompt("Qual o nome do seu herói?")
-heroXP = prompt("Qual o XP dele(a)?")
-*/
+console.log("Bem-vindo ao classifacor de Rank!\n")
 
 if (heroXP <= 1000){
     heroRank = "Ferro"
